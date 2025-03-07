173d22cb0a4303b0cfd2cbce7c1f00005bb9823f
AlejandroHinojosaRubio <hinojosa.rub@gmail.com>
2025-03-07 23:54:20 +0100

    First commit

---
+++ src/types/types.ts
@@ -0,0 +1,10 @@
+export type Experience = {
+    title: string,
+    company: string,
+    location: string,
+    from: string,
+    to: string,
+    list: string[],
+    techStack?: string[],
+    other?: string[],
+}
